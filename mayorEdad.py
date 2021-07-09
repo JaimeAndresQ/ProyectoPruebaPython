b0e55749b786a24a35579fc3e9701a21c1a48de7
JaimeAndresQ <jaime.quinones.chacon@gmail.com>
2021-07-08 23:14:51 -0500

    Agregamos válidación de edad

--- mayorEdad.py
+++ mayorEdad.py
@@ -1,5 +1,9 @@
 age = int(input("How old are you?: "))
-if age >= 18: 
-    print("You are adult in Colombia")
+while age < 0: 
+    print("It is not a valid age, please enter a valid age!!")
+    age = int(input("How old are you?: "))
 else:
-    print("You aren't adult in Colombia")
+    if age >= 18: 
+        print("You are adult in Colombia")
+    else:
+        print("You aren't adult in Colombia")
